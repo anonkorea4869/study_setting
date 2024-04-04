a0c83d6e8969d7e1ffa7c05ed22e156fa5e8d537
kimdaehun <anonkorea@gachon.ac.kr>
2024-04-04 22:06:26 +0900

    finish

--- main.py
+++ main.py
@@ -2,9 +2,29 @@ from fastapi import FastAPI, Form, Request, Response, HTTPException, Cookie, API
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 import os
+import subprocess
 
 app = FastAPI()
 templates = Jinja2Templates(directory=".")
+
+def createUser(id, pw) :
+    os.system(f"useradd -m {id}")
+    os.system(f"usermod -s /bin/bash {id}")
+    os.system(f"echo '{id}:{pw}' | chpasswd")
+
+def createKey(id) :
+    os.system(f"mkdir -p /home/{id}/.ssh/")
+    os.system(f"chmod 700 /home/{id}/.ssh/")
+    os.system(f"ssh-keygen -t rsa -b 2048 -f /home/{id}/.ssh/smart -N ''")
+    os.system(f"cp /home/{id}/.ssh/smart.pub /home/{id}/.ssh/authorized_keys")
+    os.system(f"chmod 600 /home/{id}/.ssh/authorized_keys")
+    os.system(f"chown -R {id}:{id} /home/{id}/.ssh/")
+
+    public_key = subprocess.run(["cat", f'/home/{id}/.ssh/smart'], capture_output=True, text=True)
+    public_key = str(public_key)
+    public_key = public_key[public_key.find('----') : public_key.find('-----END OPENSSH PRIVATE KEY-----') + len('-----END OPENSSH PRIVATE KEY-----')]
+
+    return public_key
 
 @app.get("/{site}.html")
 def loadSite(request: Request, site: str) :
@@ -12,9 +32,20 @@ def loadSite(request: Request, site: str) :
 
 @app.get("/api/index")
 def index(request : Request, password : str, id : str, pw : str) :
-    if password == "zeropointerlab" : 
-        os.system(f"adduser {id} -d /home/{id}; usermod -s /bin/bash {id}")
-        os.system(f"echo '{id}:{pw}' | chpasswd")
-        return templates.TemplateResponse(f"/info.html", {"request": request, "id" : id, "pw" : pw})
+    if password == "zeropointerlab" :
+        패스워드 규칙 조회
+        if len(pw) < 8 or id in pw :
+            return RedirectResponse("/index.html")
+
+        try: # 계정 이미 존재하는지
+            subprocess.run(["id", id], check=True, capture_output=True)
+            return RedirectResponse("/index.html")
+        except subprocess.CalledProcessError:
+            pass
+
+        createUser(id, pw)
+        public_key = createKey(id)
+
+        return templates.TemplateResponse(f"/info.html", {"request": request, "id" : id, "pw" : pw, "Pk" : public_key})
     else :
         return RedirectResponse("/index.html")
